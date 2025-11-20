2b2580808f99215b19ff8d24cbf7a7d8b8686ec5
Steve Tyler <steve.tyler75@gmail.com>
2025-11-20 22:15:09 +0000

    implemented store and hid cart

---
+++ src/store/cart.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialCartState = { items: [], isVisible: false, totalQuantity: 0};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: initialCartState,
+    reducers: {
+        addItem(state, action) {
+            const existingItemIndex = state.items.findIndex(item => item.id === action.payload.id);
+            state.totalQuantity++;
+            if (existingItemIndex >= 0) {
+                state.items[existingItemIndex].quantity++;
+            } else {
+                state.items.push({ ...action.payload, quantity: 1 });
+            }   
+        },
+        removeItem(state, action) {
+            const existingItemIndex = state.items.findIndex(item => item.id === action.payload);
+            if (existingItemIndex >= 0) {
+                const existingItem = state.items[existingItemIndex];
+                state.totalQuantity--;
+                if (existingItem.quantity === 1) {
+                    state.items.splice(existingItemIndex, 1);
+                }   else {
+                    state.items[existingItemIndex].quantity--;
+                }
+            }
+        },
+        toggleCart(state) {
+            state.isVisible = !state.isVisible;
+        },
+    }
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice.reducer;
